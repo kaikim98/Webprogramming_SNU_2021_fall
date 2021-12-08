2cddaa771c7f95fb38d885a382a73d5550f5e40c
kaikim98 <71422060+kaikim98@users.noreply.github.com>
2021-12-08 19:54:15 +0900

    Update index.js

--- final_individual_task/models/index.js
+++ final_individual_task/models/index.js
@@ -3,7 +3,7 @@ const User = require('./User')
 const Coin = require('./Coin')
 const Asset = require('./Asset')
 const keys = require('./keys')
-const mongoURL = 'mongodb+srv://test0:test0@testmongo.7yito.mongodb.net/CoinServer?retryWrites=true&w=majority';
+const mongoURL = 'mongodb+srv://:@testmongo.7yito.mongodb.net/CoinServer?retryWrites=true&w=majority';
 mongoose.connect(mongoURL);
 
 module.exports = {
